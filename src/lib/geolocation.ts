1120774649d1be3c3cde762b55528308071ccdc2
h4rtonohutauruk-art <h4rtonohutauruk@gmail.com>
2026-07-09 15:02:22 +0700

    added position to check

--- src/lib/geolocation.ts
+++ src/lib/geolocation.ts
@@ -3,15 +3,16 @@ import { Geolocation } from "@capacitor/geolocation";
 
 export async function getCurrentLocation() {
   console.log("call the getCurrentLocation:", Capacitor.getPlatform());
-  if (Capacitor.isNativePlatform()) {
-    console.log("masuk if berarti ini masuk ke native");
-    await Geolocation.requestPermissions();
-    return await Geolocation.getCurrentPosition();
-  }
+  // if (Capacitor.isNativePlatform()) {
+  //   console.log("masuk if berarti ini masuk ke native");
+  //   const permission =
+  //   await Geolocation.requestPermissions();
+  //   return await Geolocation.getCurrentPosition();
+  // }
 
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
+  // return new Promise((resolve, reject) => {
+  //   navigator.geolocation.getCurrentPosition(resolve, reject);
+  // });
   //   const permission = await Geolocation.checkPermissions();
 
   //   console.log(permission);
@@ -26,4 +27,29 @@ export async function getCurrentLocation() {
   //   });
 
   //   return position.coords;
+  try {
+    if (Capacitor.isNativePlatform()) {
+      console.log("masuk if berarti ini masuk ke native");
+      const permission = await Geolocation.requestPermissions();
+
+      console.log(permission);
+
+      if (permission.location !== "granted") {
+        await Geolocation.requestPermissions();
+      }
+
+      const position = await Geolocation.getCurrentPosition({
+        enableHighAccuracy: true,
+        timeout: 10000,
+      });
+
+      return position.coords;
+    }
+
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  } catch (error) {
+    console.log("Error in getCurrentLocation", error);
+  }
 }
